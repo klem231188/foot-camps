888f371ae430cba724f4d68089dde68deccd7634
klem231188 <clemtreguer@gmail.com>
2017-05-29 00:41:33 +0200

    Maquettage

--- src/app/football-camp-overview/football-camp-overview.component.ts
+++ src/app/football-camp-overview/football-camp-overview.component.ts
@@ -3,7 +3,7 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'football-camp-overview',
   templateUrl: 'football-camp-overview.component.html',
-  styleUrls: ['./football-camp-overview.component.css']
+  styleUrls: ['football-camp-overview.component.scss']
 })
 export class FootballCampOverviewComponent {
 
