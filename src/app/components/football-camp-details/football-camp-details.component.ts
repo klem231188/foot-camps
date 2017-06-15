92b5e7020ddda194d016b7d31e49c44ce522688c
klem231188 <clemtreguer@gmail.com>
2017-06-16 00:24:18 +0200

    Maquettage details

--- src/app/components/football-camp-details/football-camp-details.component.ts
+++ src/app/components/football-camp-details/football-camp-details.component.ts
@@ -1,26 +1,23 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild, AfterViewInit} from "@angular/core";
 import {FootballCamp} from "../../services/football-camp/football-camp";
 import {FootballCampService} from "../../services/football-camp/football-camp.service";
 import {ActivatedRoute, Params} from "@angular/router";
-import {MdIconRegistry} from "@angular/material";
-import {DomSanitizer} from "@angular/platform-browser";
+import {GalleryComponent} from "angular2-image-gallery";
 
 @Component({
   selector: 'football-camp-details',
   templateUrl: 'football-camp-details.component.html',
   styleUrls: ['football-camp-details.component.scss']
 })
-export class FootballCampDetailsComponent implements OnInit {
+export class FootballCampDetailsComponent implements OnInit, AfterViewInit {
 
   private footballCamp: FootballCamp = null;
 
   constructor(private route: ActivatedRoute,
-              private footballCampService: FootballCampService,
-              private mdIconRegistry: MdIconRegistry,
-              private sanitizer: DomSanitizer) {
-    mdIconRegistry.addSvgIcon('ball', sanitizer.bypassSecurityTrustResourceUrl('assets/img/ball.svg'));
+              private footballCampService: FootballCampService,) {
   }
 
+  @ViewChild(GalleryComponent) gallery: GalleryComponent;
 
   ngOnInit(): void {
     this.route
@@ -31,5 +28,12 @@ export class FootballCampDetailsComponent implements OnInit {
       .subscribe((footballCamp: FootballCamp) => {
         this.footballCamp = footballCamp
       });
+  }
+
+  ngAfterViewInit() {
+  }
+
+  onViewerChange(event) {
+    console.log(event);
   }
 }
