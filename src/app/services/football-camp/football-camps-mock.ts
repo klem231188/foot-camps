3ca6467ff98be22886ce4185cdc2c1f724666300
klem231188 <chipie23>
2017-06-20 14:01:08 +0200

    Lorient

--- src/app/services/football-camp/football-camps-mock.ts
+++ src/app/services/football-camp/football-camps-mock.ts
@@ -3,21 +3,21 @@ import {FootballCamp} from "./football-camp";
 export const FOOTBAL_CAMPS: FootballCamp[] = [
   {
     id: 0,
-    latitude: 46.7307497,
-    longitude: 3.5123698,
+    latitude: 47.7482524,
+    longitude: -3.3702449,
     ville: 'Lorient',
-    pathToImage: "./assets/img/examples/camp0.jpg",
+    pathToImage: "./assets/img/lorient/cover.jpg",
     title: "Bienvenue au Foot Camps de Lorient",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     pathToLogo: "./assets/img/lorient/logo.png"
   },
   {
     id: 1,
-    latitude: 48.7307497,
-    longitude: 0.5123698,
+    latitude: 48.3814380,
+    longitude: -4.6204040,
     ville: 'Plouzané',
     pathToImage: "./assets/img/examples/camp1.jpg",
-    title: "Bienvenue au Foot Camps de Lorient",
+    title: "Bienvenue au Foot Camps de Plouzané",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     pathToLogo: "./assets/img/plouzane/logo.png"
   }
