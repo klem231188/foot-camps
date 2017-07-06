a6d0cbdd55fd880d0ac215bdbf27755130b9fe8d
klem231188 <clemtreguer@gmail.com>
2017-07-07 00:17:49 +0200

    Maquettage

--- src/app/components/football-camp-map/football-camp-map.component.ts
+++ src/app/components/football-camp-map/football-camp-map.component.ts
@@ -12,8 +12,11 @@ export class FootballCampMapComponent implements OnInit {
 
   footballCamps: FootballCamp[];
 
+  zoom : number;
+
   constructor(private router: Router,
               private footballCampService: FootballCampService) {
+    this.zoom = window.screen.width > 960 ? 6 : 5;
   }
 
   ngOnInit(): void {
