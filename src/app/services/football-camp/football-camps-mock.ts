f299e309c3e6a102db2e662473c8ad5325f2667c
klem231188 <clemtreguer@gmail.com>
2017-07-22 10:07:55 +0200

    Maquettage plabennec

--- src/app/services/football-camp/football-camps-mock.ts
+++ src/app/services/football-camp/football-camps-mock.ts
@@ -115,7 +115,7 @@ plabennec.city = 'Plabennec';
 
 //- Overview
 plabennec.overview = new Overview()
-  .withPathToImage('./assets/img/plabennec/cover.jpg')
+  .withPathToImage('./assets/img/plabennec/overview.jpg')
   .withContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
   .withTitle('Bienvenue au Foot Camps de Plabennec');
 
