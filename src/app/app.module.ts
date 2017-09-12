4d1672fa8845fdf2d1807cfca2a1ded382e8b3e2
klem231188 <chipie23>
2017-09-12 14:05:27 +0200

    Work on search bar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {AppComponent} from "./app.component";
@@ -30,6 +30,7 @@ import {TruncatePipe} from "./pipes/truncate-pipe";
     FormsModule,
     HttpModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
     // @MaterialDesign
     AppMaterialModule,
     // ImageGallery
--- src/app/components/football-camp-locator/football-camp-locator.component.ts
+++ src/app/components/football-camp-locator/football-camp-locator.component.ts
@@ -2,13 +2,21 @@ import {Component, OnInit} from "@angular/core";
 import {FootballCampService} from "../../services/football-camp/football-camp.service";
 import {FootballCamp} from "../../services/football-camp/football-camp";
 import {ActivatedRoute, Router, Params} from "@angular/router";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'football-camp-locator',
   templateUrl: 'football-camp-locator.component.html'
 })
 export class FootballCampLocatorComponent implements OnInit {
+
   footballCamp: FootballCamp = null;
+  footballCamps: FootballCamp[];
+  //observableFootballCamps: Observable<FootballCamp[]>;
+  searchFootballCampInput: FormControl = new FormControl();
 
   constructor(private route: ActivatedRoute,
               private footballCampService: FootballCampService) {
@@ -23,5 +31,18 @@ export class FootballCampLocatorComponent implements OnInit {
       .subscribe((footballCamp: FootballCamp) => {
         this.footballCamp = footballCamp
       });
+
+    this.footballCampService
+      .getFootballCamps()
+      .then(footballCamps => this.footballCamps = footballCamps);
+
+    // this.observableFootballCamps = this.searchFootballCampInput.valueChanges
+    //   .startWith(null)
+    //   .map(value => value ? this.filter(value) : this.footballCamps.slice());
   }
+
+  // filter(val: string): FootballCamp[] {
+  //   return this.footballCamps.filter(footballCamp => footballCamp.city.toLowerCase().indexOf(val.toLowerCase()) === 0);
+  // }
+
 }
