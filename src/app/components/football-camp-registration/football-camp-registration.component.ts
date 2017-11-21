79859cc85da29d9626f4ffdaac8cd50f92375ab3
klem231188 <clemtreguer@gmail.com>
2017-11-22 00:06:34 +0100

    registration

--- src/app/components/football-camp-registration/football-camp-registration.component.ts
+++ src/app/components/football-camp-registration/football-camp-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Registration } from './registration';
 
 @Component({
   selector: 'football-camp-registration',
@@ -9,9 +10,14 @@ export class FootballCampRegistrationComponent implements OnInit {
 
   startDate: Date = new Date(2000, 0, 1);
 
+  registration: Registration = new Registration();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onConfirmRegistration(): void {
+    console.log(this.registration);
+  }
 }
---
+++ src/app/components/football-camp-registration/registration.ts
@@ -0,0 +1,12 @@
+import { Gender } from './gender.enum';
+
+export class Registration {
+    birthdate: Date;
+    email: string;
+    firstname: string;
+    gender: Gender;
+    lastname: string;
+
+    constructor() {
+    }
+}
