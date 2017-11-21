a4bdd7535657887ae26ecd3b4b5600ee3401b242
klem231188 <chipie23>
2017-11-21 17:35:02 +0100

    registration

--- src/app/components/football-camp-registration/football-camp-registration.component.ts
+++ src/app/components/football-camp-registration/football-camp-registration.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FootballCampRegistrationComponent implements OnInit {
 
+  startDate: Date = new Date(2000, 0, 1);
+
   constructor() { }
 
   ngOnInit() {
