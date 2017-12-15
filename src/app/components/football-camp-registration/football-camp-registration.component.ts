808b2b5b7e0a1b54b28f8eae4eccdeb48e992d9a
klem231188 <clemtreguer@gmail.com>
2017-12-15 23:58:33 +0100

    Feature registration stepper

--- src/app/components/football-camp-registration/football-camp-registration.component.ts
+++ src/app/components/football-camp-registration/football-camp-registration.component.ts
@@ -1,23 +1,25 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild, AfterViewInit} from '@angular/core';
 import {Registration} from './registration';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {FootballCampService} from '../../services/football-camp/football-camp.service';
 import {FootballCamp} from '../../services/football-camp/football-camp';
 import {ActivatedRoute, Params} from '@angular/router';
+import {MatVerticalStepper} from '@angular/material';
 
 @Component({
   selector: 'football-camp-registration',
   templateUrl: './football-camp-registration.component.html',
   styleUrls: ['./football-camp-registration.component.scss']
 })
-export class FootballCampRegistrationComponent implements OnInit, OnDestroy {
+export class FootballCampRegistrationComponent implements OnInit, AfterViewInit, OnDestroy {
   startDate: Date = new Date(2000, 0, 1);
   registration: Registration = new Registration();
-  isLinear = true;
   registrationFormGroup: FormGroup;
   paymentFormGroup: FormGroup;
 
   footballCamp: FootballCamp;
+
+  @ViewChild('stepper') stepper: MatVerticalStepper;
 
   constructor(private formBuilder: FormBuilder,
               private route: ActivatedRoute,
@@ -29,7 +31,7 @@ export class FootballCampRegistrationComponent implements OnInit, OnDestroy {
       registrationController: ['', Validators.minLength(0)]
     });
     this.paymentFormGroup = this.formBuilder.group({
-      paymentController: ['', Validators.required]
+      paymentController: ['', Validators.minLength(0)]
     });
 
     this.route
@@ -40,7 +42,19 @@ export class FootballCampRegistrationComponent implements OnInit, OnDestroy {
       })
       .subscribe((footballCamp: FootballCamp) => {
         this.footballCamp = footballCamp;
+        setTimeout(() => {
+          this.stepper.selectionChange.asObservable().subscribe((selection) => {
+            if (selection.selectedIndex === 2) {
+              this.stepper._steps.forEach((step) => step.editable = false);
+            }
+          })
+        }, 10);
       });
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.theStepper)
+    // this.theStepper.selectionChange.asObservable().subscribe(event => {console.log(event)})
   }
 
   ngOnDestroy(): void {
