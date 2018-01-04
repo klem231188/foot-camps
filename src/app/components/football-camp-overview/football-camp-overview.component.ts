0dab70acb86679c63b96793184d7914550ee2bfc
klem231188 <chipie23>
2018-01-04 17:40:32 +0100

    Using firestore to retrieve camps... To be continued...

--- src/app/components/football-camp-overview/football-camp-overview.component.ts
+++ src/app/components/football-camp-overview/football-camp-overview.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {FootballCamp} from '../../services/football-camp/football-camp';
 import {FootballCampService} from '../../services/football-camp/football-camp.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import {Session} from '../../models/session';
 import {Registration} from '../../models/registration';
+import {FootballCamp} from '../../models/football-camp';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'football-camp-overview',
@@ -16,25 +18,32 @@ export class FootballCampOverviewComponent implements OnInit {
 
   footballCamp: FootballCamp = null;
 
-  session: Session;
+  session: Session = null;
 
-  registrations: Registration[];
+  registrations: Registration[] = null;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private footballCampService: FootballCampService,
               private afs: AngularFirestore) {
-
   }
 
   ngOnInit(): void {
+    this.afs.doc<FootballCamp>('camps/SaZez1BrGCbuOdduv2Km').valueChanges().subscribe((footballCamp: FootballCamp) => {
+        console.log(footballCamp);
+      }
+    );
+
     this.route
       .params
-      .switchMap((params: Params) => {
-        return this.footballCampService.getFootballCamp(+params['id']);
-      })
-      .subscribe((footballCamp: FootballCamp) => {
-        this.footballCamp = footballCamp;
+      .map((params: Params) => {
+        console.log(params);
+        const campId: string = params['id']
+        this.afs.doc<FootballCamp>('camps/' + campId).valueChanges().subscribe((footballCamp: FootballCamp) => {
+            console.log(footballCamp);
+            this.footballCamp = footballCamp;
+          }
+        )
       });
 
     const sessionDoc: AngularFirestoreDocument<Session> = this.afs.doc<Session>('sessions/Lp4PUMsZrJznyssU0hpI');
