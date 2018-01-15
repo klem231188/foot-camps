3fe047fd765db33e73412bc1cce552fc8f44027b
klem231188 <chipie23>
2018-01-15 14:29:32 +0100

    locale fr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, LOCALE_ID} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {AppComponent} from './app.component';
+import {registerLocaleData} from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import { AppRoutingModule } from './app-routing.module';
-import { AppMaterialModule } from './app.material.module';
-import { AgmCoreModule } from '@agm/core';
-import { FootballCampLocatorComponent } from './components/football-camp-locator/football-camp-locator.component';
-import { FootballCampHeaderComponent } from './components/football-camp-header/football-camp-header.component';
-import { FootballCampDetailsComponent } from './components/football-camp-details/football-camp-details.component';
-import { FootballCampMapComponent } from './components/football-camp-map/football-camp-map.component';
-import { FootballCampOverviewComponent } from './components/football-camp-overview/football-camp-overview.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { Angular2ImageGalleryModule } from './components/angular2-image-gallery';
-import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment';
-import { SafeHtmlPipe } from './pipes/safe-html-pipe';
-import { TruncatePipe } from './pipes/truncate-pipe';
-import { FootballCampRegistrationComponent } from './components/football-camp-registration/football-camp-registration.component';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { FootballCampLoginComponent } from './components/football-camp-login/football-camp-login.component';
-import { FootballCampShouldConnectDialogComponent } from './components/football-camp-should-connect-dialog/football-camp-should-connect-dialog.component';
-import {AngularFirestoreModule} from "angularfire2/firestore";
+import {AppRoutingModule} from './app-routing.module';
+import {AppMaterialModule} from './app.material.module';
+import {AgmCoreModule} from '@agm/core';
+import {FootballCampLocatorComponent} from './components/football-camp-locator/football-camp-locator.component';
+import {FootballCampHeaderComponent} from './components/football-camp-header/football-camp-header.component';
+import {FootballCampDetailsComponent} from './components/football-camp-details/football-camp-details.component';
+import {FootballCampMapComponent} from './components/football-camp-map/football-camp-map.component';
+import {FootballCampOverviewComponent} from './components/football-camp-overview/football-camp-overview.component';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
+import {Angular2ImageGalleryModule} from './components/angular2-image-gallery';
+import {AngularFireModule} from 'angularfire2';
+import {environment} from '../environments/environment';
+import {SafeHtmlPipe} from './pipes/safe-html-pipe';
+import {TruncatePipe} from './pipes/truncate-pipe';
+import {FootballCampRegistrationComponent} from './components/football-camp-registration/football-camp-registration.component';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {FootballCampLoginComponent} from './components/football-camp-login/football-camp-login.component';
+import {FootballCampShouldConnectDialogComponent} from './components/football-camp-should-connect-dialog/football-camp-should-connect-dialog.component';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   imports: [
@@ -67,7 +71,8 @@ import {AngularFirestoreModule} from "angularfire2/firestore";
     FootballCampLoginComponent,
     FootballCampShouldConnectDialogComponent
   ],
-  providers: [AngularFireAuth],
+  providers: [AngularFireAuth, {provide: LOCALE_ID, useValue: 'fr'}],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
