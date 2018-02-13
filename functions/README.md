cb68bb42679bd53ee1ab9aa06345a4a9f995abd8
klem231188 <chipie23>
2018-02-13 13:27:40 +0100

    First functions.

--- functions/README.md
+++
@@ -1,7 +0,0 @@
-Tuto : https://medium.com/netscape/firebase-cloud-functions-with-typescript-and-webpack-7781c882a05b
-
-```
-webpack
-firebase deploy --only functions
-firebase serve --only functions
-```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,44 @@
-import * as functions from 'firebase-functions'
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
 import * as admin from 'firebase-admin'
-import * as ImportDataToFirestore from './import-data-to-firestore'
+import * as functions from 'firebase-functions'
+import {FootballCamp} from '../../src/app/models/football-camp';
 
-admin.initializeApp(functions.config().firebase)
+// Create and Deploy Cloud Function with TypeScript using script that is
+// defined in functions/package.json:
+//    cd functions
+//    npm run deploy
+admin.initializeApp(functions.config().firebase);
 
-export const importDataToFirestore = ImportDataToFirestore.listener
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const db = admin.firestore();
+  const camps = db.collection('camps');
+
+  const bourgBlancCamp: FootballCamp = new FootballCamp();
+  bourgBlancCamp.city = 'Bourg-Blanc';
+  bourgBlancCamp.latitude = 48.4926696;
+  bourgBlancCamp.longitude = -4.508196;
+
+  const result = {};
+  Object.keys(bourgBlancCamp).map(key => result[key] = bourgBlancCamp[key]);
+
+  camps.add(result);
+  response.send('Hello from Firebase!\n\n');
+});
+
