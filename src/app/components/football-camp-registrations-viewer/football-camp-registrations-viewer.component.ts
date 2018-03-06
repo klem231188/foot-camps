9660b481cafadad9408150556a0579c791935cda
klem231188 <chipie23>
2018-03-06 18:13:21 +0100

    Working on registrations-viewer component

--- src/app/components/football-camp-registrations-viewer/football-camp-registrations-viewer.component.ts
+++ src/app/components/football-camp-registrations-viewer/football-camp-registrations-viewer.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
 import {RegistrationService} from '../../services/registration/registration.service';
 import {SessionService} from '../../services/session/session.service';
 import {FootballCampService} from '../../services/football-camp/football-camp.service';
+import {UserService} from '../../services/user/user.service';
+import {User} from '../../models/user';
+import {Session} from '../../models/session';
 
 @Component({
   selector: 'app-football-camp-registrations-viewer',
@@ -10,12 +14,30 @@ import {FootballCampService} from '../../services/football-camp/football-camp.se
 })
 export class FootballCampRegistrationsViewerComponent implements OnInit {
 
-  constructor(private footballCampService: FootballCampService,
+  user: User = null;
+
+  sessions: Session[] = null;
+
+  constructor(private angularFireAuth: AngularFireAuth,
+              private footballCampService: FootballCampService,
+              private userService: UserService,
               private sessionService: SessionService,
               private registrationService: RegistrationService) {
   }
 
   ngOnInit() {
+    this.angularFireAuth.authState
+      .switchMap<firebase.User, User>((firebaseUser) => {
+        return this.userService.getUser(firebaseUser.uid);
+      })
+      .switchMap<User, Session[]>((user) => {
+        this.user = user;
+        // role == ADMIN
+        return this.sessionService.getSessions();
+      })
+      .subscribe((sessions) => {
+        this.sessions = sessions
+      });
   }
 
 }
