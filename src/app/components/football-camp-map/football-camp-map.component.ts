16cd3b0305af1d51432881d331d83fbb0ed42f86
klem231188 <chipie23>
2018-05-31 10:14:11 +0200

    Modifying GPS coordinate

--- src/app/components/football-camp-map/football-camp-map.component.ts
+++ src/app/components/football-camp-map/football-camp-map.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {AngularFirestore} from 'angularfire2/firestore';
 import {FootballCamp} from '../../models/football-camp';
-import {Observable} from 'rxjs';
 
 
 @Component({
@@ -17,7 +15,8 @@ export class FootballCampMapComponent implements OnInit, OnDestroy {
   zoom: number;
 
   constructor(private router: Router) {
-    this.zoom = window.screen.width > 960 ? 6 : 5;
+    //this.zoom = window.screen.width > 960 ? 6 : 5;
+    this.zoom = window.screen.width > 960 ? 8 : 7;
   }
 
   ngOnInit(): void {
