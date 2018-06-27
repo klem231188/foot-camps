b9663a76386a4e12440489f2a1e3029fced57a9d
klem231188 <clemtreguer@gmail.com>
2018-06-27 23:29:19 +0200

    1. [Ecran Inscription][Documents] Gestion des documents

--- src/app/components/football-camp-file-upload/football-camp-file-upload.component.ts
+++ src/app/components/football-camp-file-upload/football-camp-file-upload.component.ts
@@ -1,29 +1,38 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {AngularFireStorage, AngularFireUploadTask} from 'angularfire2/storage';
-import {AngularFirestore} from 'angularfire2/firestore';
 import {BehaviorSubject, Observable} from 'rxjs';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-football-camp-file-upload',
   templateUrl: './football-camp-file-upload.component.html',
   styleUrls: ['./football-camp-file-upload.component.scss']
 })
-export class FootballCampFileUploadComponent implements OnInit {
-
-  @Input() type: FileUploadType;
-  title: string;
-  inputId: string;
-  uploaded: BehaviorSubject<boolean>;
-  task: AngularFireUploadTask;
-  percentage: Observable<number>;
+export class FootballCampFileUploadComponent implements OnInit, OnDestroy {
+  // ----------------------------
+  // Fields
+  // ----------------------------
   downloadURL: string;
+  inputId: string;
+  percentage: Observable<number>;
+  subscriptions: Subscription[];
+  task: AngularFireUploadTask;
+  title: string;
+  @Input() type: FileUploadType;
+  uploaded: BehaviorSubject<boolean>;
 
-  constructor(private storage: AngularFireStorage,
-              private db: AngularFirestore) {
+  // ----------------------------
+  // Constructor
+  // ----------------------------
+  constructor(private storage: AngularFireStorage) {
   }
 
+  // ----------------------------
+  // Lifecycle hooks
+  // ----------------------------
   ngOnInit(): void {
     this.uploaded = new BehaviorSubject(false);
+
     switch (this.type) {
       case FileUploadType.ASSURANCE_SCOLAIRE :
         this.title = 'Attestation assurance scolaire';
@@ -42,39 +51,60 @@ export class FootballCampFileUploadComponent implements OnInit {
         this.inputId = 'photo_identite_id';
         break;
     }
+
+    this.subscriptions = [];
   }
 
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  // ----------------------------
+  // Methods
+  // ----------------------------
   startUpload(event: FileList) {
     console.log('startUpload');
     const file = event.item(0);
 
+    // No file has been selected
     if (!file) {
       return;
     }
 
+    // Say to "uploaded" status observers that file is not yet uploaded
     this.uploaded.next(false);
 
-    // Client-side validation example
-    if (file.type.split('/')[0] !== 'image') {
-      console.error('unsupported file type :( ');
-      return;
-    }
-
+    // Define file path
     const path = `uploads/${new Date().getTime()}_${file.name}`;
-    console.log(path);
 
-    const customMetadata = {app: 'My AngularFire-powered PWA!'};
-    this.task = this.storage.upload(path, file, {customMetadata});
+    // Create an upload task
+    this.task = this.storage.upload(path, file);
 
+    // Keep a variable on upload percentage observable for the progress bar
     this.percentage = this.task.percentageChanges();
-    this.task.snapshotChanges()
+
+    this.task
+      .snapshotChanges()
       .subscribe((snap) => {
         if (snap.bytesTransferred === snap.totalBytes) {
-          this.db.collection('photos').add({path, size: snap.totalBytes});
+          // Upload is now finished
+
+          // Say to "uploaded" status observers that file has been uploaded
           this.uploaded.next(true);
-          this.storage.ref(path).getDownloadURL().subscribe(url => {
-            this.downloadURL = url
-          });
+
+          // TODO: move following line when registration is done
+          // this.db.collection('photos').add({path, size: snap.totalBytes});
+
+          // Save upload full URL into a variable
+          const subscription = this.storage
+            .ref(path)
+            .getDownloadURL()
+            .subscribe(url => {
+              this.downloadURL = url;
+            });
+          this.subscriptions.push(subscription);
         }
       });
   }
