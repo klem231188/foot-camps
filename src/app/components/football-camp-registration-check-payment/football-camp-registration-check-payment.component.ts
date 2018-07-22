1cec01a7a7420fa0f0c47c561d0a0b6d87eb9557
klem231188 <clemtreguer@gmail.com>
2018-07-22 23:37:54 +0200

    1. [Ecran Inscription] Clean & split registration form in component

--- src/app/components/football-camp-registration-check-payment/football-camp-registration-check-payment.component.ts
+++ src/app/components/football-camp-registration-check-payment/football-camp-registration-check-payment.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FootballCampService} from '../../services/football-camp/football-camp.service';
+import {Observable} from 'rxjs/index';
+import {FootballCamp} from '../../models/football-camp';
+import {SessionService} from '../../services/session/session.service';
+import {Session} from '../../models/session';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-football-camp-registration-check-payment',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FootballCampRegistrationCheckPaymentComponent implements OnInit {
 
-  constructor() { }
+  @Input() campId: string;
+  @Input() sessionId: string;
+
+  footballCamp$: Observable<FootballCamp>;
+  session$: Observable<Session>;
+
+  constructor(
+    private footballCampService: FootballCampService,
+    private sessionService: SessionService
+  ) {
+  }
 
   ngOnInit() {
+    console.log('FootballCampRegistrationCheckPaymentComponent.ngOnInit()');
+
+    this.footballCamp$ = this.footballCampService.getFootballCamp(this.campId);
+    this.session$ = this.sessionService
+      .getSessionsFromCampId(this.campId)
+      .pipe(
+        map((sessions: Session[]) => {
+          return sessions.find(session => session && session.id === this.sessionId)
+        })
+      );
   }
 
 }
